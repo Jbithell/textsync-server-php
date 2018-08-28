17ef932db2ba9188e5024a453d4265b8b43fb845
James Bithell <Jbithell@users.noreply.github.com>
2018-08-28 20:10:54 +0100

    Correct use of POST (based on https://stackoverflow.com/questions/19004783/reading-json-post-using-php)

--- examples/authenticate.php
+++ examples/authenticate.php
@@ -6,15 +6,14 @@ $textsync = new Textsync\Textsync([
   'key' => 'your:key'
 ]);
 
-if (!isset($_POST['docId'])) die("Document id not specified");
+$payload = json_decode(file_get_contents('php://input'), true); //We can't use post as we might normally because the client passes content type application/json
+
+if (!isset($payload['docId'])) die("Please pass a document id"); //Generic response to a request to this url
 
 if (true) { //Decide here what permissions this user might have for this doc id
-    $auth_data = $textsync->authenticate([ 'docId' => $_POST['docId'], 'permissions' => ['READ','WRITE'], 'tokenExpiry' => 1200]);
+    $auth_data = $textsync->authenticate([ 'docId' => $payload['docId'], 'permissions' => ['READ','WRITE'], 'tokenExpiry' => 1200]);
 }
-
 if ($auth_data['status'] != 200) throw new Exception("Error authorizing");
-
-
 header("Access-Control-Allow-Origin: *");
 header('Content-type: application/json');
 echo json_encode($auth_data['body']);
